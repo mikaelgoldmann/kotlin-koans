2b258fbd473dcb055e1b7ae5ead3a9dc82c741bf
Mikael Goldmann <migo@spotify.com>
2018-02-27 21:18:38 +0100

    solve generics

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -20,11 +20,15 @@ fun task41(): Nothing = TODO(
 )
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return this.partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+}
+
+fun <T, C: MutableCollection<T>> Iterable<T>.partitionTo(yes: C, no: C, p: (T)->Boolean): Pair<C, C> {
+    this.forEach { if (p(it)) yes.add(it) else no.add(it) }
+    return Pair(yes, no)
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    //task41()
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
